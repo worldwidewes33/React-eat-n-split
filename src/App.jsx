5e81b67c92899402c425750c370042f9c9877246
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-03-13 11:42:01 -0600

    created and styled user avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return <UserAvatar />;
+}
+
+function UserAvatar() {
+  return (
+    <div className="user-info">
+      <img src="https://i.pravatar.cc/50" alt="user avi" />
+      <div className="content">
+        <h3>Wesley Austin</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+      <button className="btn">Select</button>
+    </div>
+  );
 }
 
 export default App;
