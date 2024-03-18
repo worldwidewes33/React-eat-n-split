d20b03683e8588456ce00161c1512dfad4ef8e3f
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-03-18 11:05:44 -0600

    made all user avatars display

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
+import PropTypes from "prop-types";
+
+const users = [
+  {
+    id: 14569,
+    name: "Taylor Green",
+    balance: 7,
+    photo_url: "https://i.pravatar.cc/50?u=14569",
+  },
+  {
+    id: 39532,
+    name: "Dustin Jackson",
+    balance: -20,
+    photo_url: "https://i.pravatar.cc/50?u=39532",
+  },
+  {
+    id: 95394,
+    name: "Walter Brown",
+    balance: 0,
+    photo_url: "https://i.pravatar.cc/50?u=95394",
+  },
+];
+
 function App() {
-  return <UserAvatar />;
+  const userAvatars = users.map((user) => (
+    <li key={user.id}>
+      <UserAvatar user={user} />
+    </li>
+  ));
+
+  return <ul>{userAvatars}</ul>;
 }
 
-function UserAvatar() {
+function UserAvatar({ user }) {
   return (
     <div className="user-info">
-      <img src="https://i.pravatar.cc/50" alt="user avi" />
+      <img src={user.photo_url} alt={user.name} />
       <div className="content">
-        <h3>Wesley Austin</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h3>{user.name}</h3>
+        {user.balance > 0 && (
+          <p className="green">{`${user.name} owes you $${user.balance}`}</p>
+        )}
+        {user.balance == 0 && <p>{`${user.name} and you are even`}</p>}
+        {user.balance < 0 && (
+          <p className="red">{`You owe ${user.name} $${Math.abs(
+            user.balance
+          )}`}</p>
+        )}
       </div>
       <button className="btn">Select</button>
     </div>
   );
 }
+
+UserAvatar.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default App;
